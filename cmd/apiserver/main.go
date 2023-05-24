7ea821992810928acc99418cc37dd3599e238570
Danila Egorov <c4nsero@mail.ru>
2023-05-24 04:53:42 +0300

    update main: add flag
    Перешёл с os.Args[1] на flag.BoolFlag()
    Добавил вывод типа хранилища при запуске

--- cmd/apiserver/main.go
+++ cmd/apiserver/main.go
@@ -3,31 +3,31 @@ package main
 import (
 	"LinkCutter/internal/handler"
 	"LinkCutter/internal/models"
+	"flag"
 	"net/http"
-	"os"
 
 	"github.com/sirupsen/logrus"
 )
 
 func main() {
 
-	// Настройка логгера и вывод записи о начале работы
+	// Настройка логгера
 	logrus.SetFormatter(new(logrus.JSONFormatter))
-	logrus.Info("Start API")
 
-	// Проверка количества параметров
-	if len(os.Args) == 1 {
-		logrus.Fatal("Memory type not expected")
-	}
+	var dbFlag bool
+	flag.BoolVar(&dbFlag, "db", false, "Run with DB postgres")
+	flag.Parse()
 
 	// Проверка флага места хранения ссылок
-	if os.Args[1] == "Cache" {
+	if !dbFlag {
+		logrus.Info("Start API with In-Memory storage")
 		mapShort := make(map[string]models.Url)
 		mapLong := make(map[string]models.Url)
 		http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 			handler.CacheHandler(w, r, mapShort, mapLong)
 		})
 	} else {
+		logrus.Info("Start API with PostgreSQL storage")
 		http.HandleFunc("/", handler.DBHandler)
 	}
 
